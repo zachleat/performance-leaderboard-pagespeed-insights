770a7f6585d700485c0205b948c5a53b4c38b32f
Zach Leatherman <zachleatherman@gmail.com>
2023-02-12 20:33:29 -0600

    A few more TODOs

--- src/LighthousePagespeedInsights.js
+++ src/LighthousePagespeedInsights.js
@@ -1,7 +1,8 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function(pageUrl) {
-	// LIMITS https://stackoverflow.com/questions/37122041/pagespeed-insights-api-limits
+	// TODO handle API limits https://stackoverflow.com/questions/37122041/pagespeed-insights-api-limits
+  // TODO add support for `key` api keys
   let fetchUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?strategy=mobile&category=accessibility&category=best-practices&category=performance&category=seo&url=${encodeURIComponent(pageUrl)}`;
 
   return EleventyFetch(fetchUrl, {
